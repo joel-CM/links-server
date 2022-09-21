efb17b02cb8b3eda4ae573607da893251b0b0825
joel-cM <joelchavezm19@gmail.com>
2022-09-21 00:27:00 -0300

    add morgan middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import routes from "./routes/index.routes.js";
 import db from "./database/db.js";
 
@@ -10,6 +11,7 @@ app.set("port", process.env.PORT || 3001);
 
 // middlewares
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(cors("*"));
 
 // route
