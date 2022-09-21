bd904bf0d93b02eb4447c7578a22f0dd462524e8
joel-cM <joelchavezm19@gmail.com>
2022-09-21 00:49:05 -0300

    add getAll Links controller

--- src/controllers/Link.controller.js
+++ src/controllers/Link.controller.js
@@ -4,9 +4,11 @@ import handleResponse from "../helpers/handleResponse.js";
 
 const controller = {};
 
-controller.getAllLinks = (_, res) => {
-  db.query(userQuery.selectAllLinks, (err, data) => {
-    if (err) handleResponse(res, err.message, true);
+controller.getAllLinks = (req, res) => {
+  const idUser = req.token.id;
+
+  db.query(linkQuery.selectAllLinks, [idUser], (err, data) => {
+    if (err) return handleResponse(res, 500, err.message, true);
     res.json(data);
   });
 };
@@ -16,11 +18,16 @@ controller.createLink = (req, res) => {
   const idUser = req.token.id;
 
   db.query(linkQuery.createLink, [dirLink, idUser], (err, data) => {
-    if (err) handleResponse(res, err.message, true);
+    if (err) return handleResponse(res, 500, err.message, true);
     if (!data.affectedRows) {
-      handleResponse(res, "Link was not created: Unknown error", true);
+      return handleResponse(
+        res,
+        500,
+        "Link was not created: Unknown error",
+        true
+      );
     }
-    handleResponse(res, "link created!");
+    handleResponse(res, 201, "link created!");
   });
 };
 
@@ -30,9 +37,11 @@ controller.deleteLink = (req, res) => {
 
   try {
     db.query(linkQuery.deleteLink, [idLink, idUser], (err, data) => {
-      if (err) handleResponse(res, err.message, true);
-      if (!data.affectedRows) handleResponse(res, "Link not found!", true);
-      handleResponse(res, `link deleted: ${idLink}`);
+      if (err) return handleResponse(res, 500, err.message, true);
+      if (!data.affectedRows) {
+        return handleResponse(res, 404, "Link not found!", true);
+      }
+      handleResponse(res, 200, `link deleted: ${idLink}`);
     });
   } catch (e) {
     console.log(e);
@@ -45,9 +54,9 @@ controller.updateLink = (req, res) => {
   const updatedLink = req.body.link.trim();
 
   db.query(linkQuery.updateLink, [updatedLink, idLink, idUser], (err, data) => {
-    if (err) handleResponse(res, err.message, true);
-    if (!data.affectedRows) handleResponse(res, "Link not found!", true);
-    handleResponse(res, `Link updated: ${idLink}`);
+    if (err) return handleResponse(res, 500, err.message, true);
+    if (!data.affectedRows) handleResponse(res, 404, "Link not found!", true);
+    handleResponse(res, 200, `Link updated: ${idLink}`);
   });
 };
 
