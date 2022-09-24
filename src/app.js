f4606491414b141caeb9c5adb0bc050fa7f687de
joel-cM <joelchavezm19@gmail.com>
2022-09-24 20:48:12 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.set("port", process.env.PORT || 3001);
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors("*"));
+app.use(cors());
 
 // route
 app.use("/", routes);
