8fd93b5ece05107decdbdc9662b1a3f73680f9f8
joel-cM <joelchavezm19@gmail.com>
2022-09-24 20:54:21 -0300

    some fixes

--- src/app.js
+++ src/app.js
@@ -12,7 +12,16 @@ app.set("port", process.env.PORT || 3001);
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 // route
 app.use("/", routes);
